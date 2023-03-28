1016c11a6810134400cc3a1d46d6bd16d329cdda
Ludo-G7 <ludovic.garel@hotmail.fr>
2023-03-28 17:01:52 +0200

    Affichage des sorties

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'main_home')]
-    public function connection(): Response
+    #[Route('/accueil', name: 'main_home')]
+    public function home(SortieRepository $sortieRepository): Response
     {
-        return $this->render('main/home.html.twig');
+        $sortie = $sortieRepository->findAll();
+        dump($sortie);
+
+        return $this->render('main/home.html.twig', [
+            'sortie'=>$sortie
+        ]);
     }
 }
