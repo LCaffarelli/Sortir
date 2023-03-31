b61df312cac62d58f5ef21be555193241e014b12
lcaffarelli2022 <laetitia.caffarelli@gmail.com>
2023-03-31 15:43:07 +0200

    ajout de début d'ajout d'image

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,22 +12,40 @@ use App\Repository\SortieRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 
 #[Route('/user/', name: 'user_')]
 class UserController extends AbstractController
 {
     #[Route('profil', name: 'profil')]
-    public function profil(EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher, Request $request)
+    public function profil(EntityManagerInterface $entityManager, SluggerInterface $slugger,UserPasswordHasherInterface $userPasswordHasher, Request $request)
     {
         $user = $this->getUser();
         $formUpdate = $this->createForm(UpdateProfileType::class, $user);
         $formUpdate->handleRequest($request);
 
         if ($formUpdate->isSubmitted() && $formUpdate->isValid()) {
+            $img =$formUpdate->get('image')->getData();
+            if ($img) {
+                $originalFilename = pathinfo($img->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $img->guessExtension();
+
+                try {
+                    $img->move($this->getParameter('images_directory'), $newFilename);
+
+                } catch (FileException $e) {
+
+                }
+
+            }
+            $user->setImage($newFilename);
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
